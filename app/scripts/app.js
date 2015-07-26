f4c3b30c30674b3aed9b9a5bcb38ac1ec724bf3e
shahein.moussavi@izenda.com <shaheinm@gmail.com>
2015-07-26 06:20:47 -0400

    Initial commit, customized DriveCeption for UIRIMAL

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,29 +16,23 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var app = document.querySelector('#app');
   
   // Configure app parameters here
-  app.backendUrl = 'https://script.google.com/macros/s/AKfycbwUTpDFOpFxblYhrK8g7NN_IxbhhOTpzCZJo3ZNAFXWAPVlOPKA/exec';
-  app.folders = [{folderId: '0B0CnV_gvF2TgNm1RY2VQQVk5VTA', title: 'Articoli - IT'},
-                 {folderId: '0B0CnV_gvF2TgMDNINGZ3QnZsR0U', title: 'Articles - EN'}];
+  app.backendUrl = 'https://script.google.com/macros/s/AKfycbxFBtNjlSqUBKN0Y2SDZLl4V1k7_p0qOxnpmk6IDLBT9BFt11f-/exec';
+  app.folders = [{folderId: '0B9SGRkeQnLXyfnByZGMtYjJxZHB3ZE51QTNWQWtsaU8zLVFWZ01nLUZfUUJPWVVBcXJNNkE', title: 'Conference Resources'},
+                 {folderId: '0B9SGRkeQnLXyflhrcG1VMU1VWFRIeFNSWjh1SDdWVmUtdFltSFYwYzRCOFlPODNhREtDTVU', title: 'Background Guides'}];
   //Optional settings, comment/remove lines if not needed 
-  app.aboutFileId = '1HNhQgOjP4xXTPMKEW94tz54baijIBH4XiLaM4-hn0u8';
-  app.socials = [{'name':'Google +',
-                   'url':'https://plus.google.com/+MauroSmokybobSolcia'},
-                 {'name':'Youtube',
-                   'url':'https://www.youtube.com/user/hackbob84'},
-                 {'name':'GitHub',
-                   'url':'https://github.com/SmokyBob/'},
-                 {'name':'Donate - PayPal',
-                   'url':'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=F7WPXSVUAJKT4&lc=IT&item_name=Mauro%20Smokybob%20Solcia&item_number=Offer%20me%20a%20Big%20Coffee&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted'},
-                 {'name':'Engeene.it',
-                   'url':'http://www.engeene.it/author/mauro-solcia/'},
-                 {'name':'Google Developers Italia - YT PlayList',
-                   'url':'http://youtu.be/QRsXBXm3-UM?list=PLOU2XLYxmsIKU5doDlNwQlq7OO6hAY8K-'},
-                 {'name':'Curriculum Vitae',
-                   'url':'https://docs.google.com/document/d/1lMAhJEki3K9p52BAJpgCyXMrIZ6gNi22qqom-PTUdmg/edit?usp=sharing'}
+  app.aboutFileId = 'linkToWeb';
+  app.socials = [{'name':'Facebook',
+                   'url':'http://facebook.com/uirimal'},
+                 {'name':'Twitter',
+                   'url':'https://www.twitter.com/uirimal'},
+                 {'name':'Google +',
+                   'url':'https://plus.google.com/+UIRIMAL'},
+                 {'name':'YouTube',
+                   'url':'https://www.youtube.com/c/UIRIMAL'}
                 ];
   
   // Bound to the main headed panel title 
-  app.blogTitle = 'DriveCeption';
+  app.blogTitle = 'Delegate Resources';
   app.showSocials = (app.socials && app.socials.length > 0);
 
   app.displayInstalledToast = function() {
